e754c0043076dd666007a9c500653ce8ed51f2df
chloebethcanon <chloebethcanon@gmail.com>
2015-12-20 16:30:24 -0600

    added basic code in resume model to display experience, education and capstone data

--- app/models/Resume.rb
+++ app/models/Resume.rb
@@ -14,11 +14,19 @@ class Resume < Prawn::Document
   end
 
   def experience
-
+    font("Helvetica", size: 12, style: :italic)
+    text 'Experience:'
+    @resume_data[:experiences].each do |experience|
+      text "#{experience[:company].capitalize}: #{experience[:job_title]}"
+    end
   end
 
   def education
-
+    font("Helvetica", size: 12, style: :italic)
+    text 'Education:'
+    @resume_data[:educations].each do |education|
+      text "#{education[:school].capitalize}: #{education[:degree]}"
+    end
   end
 
   def skills
@@ -27,10 +35,13 @@ class Resume < Prawn::Document
     @resume_data[:skills].each do |skill|
       text "#{skill[:skill_name].capitalize}"
     end
-
   end
 
   def capstone
-
+    font("Helvetica", size: 12, style: :italic)
+    text 'Capstones:'
+    @resume_data[:capstones].each do |capstone|
+      text "#{capstone[:name].capitalize}: #{capstone[:description]}"
+    end
   end
 end
