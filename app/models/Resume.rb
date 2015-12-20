a6f21d1c602280a09aaa8675a35e71da2e2def09
MVanderwood <m.b.vanderwood@gmail.com>
2015-12-20 14:37:12 -0600

    Added PORO Resume Model and skeleton methods.

---
+++ app/models/Resume.rb
@@ -0,0 +1,32 @@
+class Resume < Prawn::Document
+  def initialize(resume_data)
+    super()
+    @resume_data = resume_data
+    header
+    experience
+    education
+    skills
+    capstone
+  end
+
+  def header
+
+  end
+
+  def experience
+
+  end
+
+  def education
+
+  end
+
+  def skills
+
+  end
+
+  def capstone
+
+  end
+  
+end
