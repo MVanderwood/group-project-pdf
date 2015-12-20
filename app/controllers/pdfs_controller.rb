5665169d5e5d811b82c0e8c4f8803323af1e7188
MVanderwood <m.b.vanderwood@gmail.com>
2015-12-20 15:39:16 -0600

    Fixed some things up

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -1,11 +1,10 @@
 class PdfsController < ApplicationController
-
   def show
     respond_to do |format|
       format.html
       format.pdf do
-        pdf = Prawn::Document.new
-        send_data pdf.render, filename: 'report.pdf', type: 'application/pdf', disposition: "inline"
+        pdf = Resume.new({blank: 0})
+        send_data pdf.render, filename: 'resume.pdf', type: 'application/pdf', disposition: "inline"
       end
     end
   end
