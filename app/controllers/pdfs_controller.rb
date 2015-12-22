770e7990328b479b81aaa59a6f600c9a491f0230
chloebethcanon <chloebethcanon@gmail.com>
2015-12-21 19:29:25 -0600

    added methods to Student model

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -3,7 +3,7 @@ class PdfsController < ApplicationController
     respond_to do |format|
       format.html
       format.pdf do
-        pdf = Resume.new({first_name: "Andre 3000", last_name: "Smith", email: "andre3000@email.com",phone: "555-555-5555", student_id: 1, skills: [{ skill_name: "rap" }, { skill_name: "awesomeness" }], experiences: [{job_title: "music-maker", company: "Outkast"}, {job_title: "actor", company: "Warner Bros"}],educations: [{degree: "music-performance", school: "chill-university"}, {degree: "performance-art", school: "depaul"}], capstones: [{name: "hey-ya", description: "hit-song"}, {name: "sorry-miss-jackson", description: "other-hit-song"}]})
+        pdf = Resume.new(Student.find_by(id: params[:id]))
         send_data pdf.render, filename: 'resume.pdf', type: 'application/pdf', disposition: "inline"
       end
     end
--- app/models/Student.rb
+++ app/models/Student.rb
@@ -1,13 +1,25 @@
 class Student
 
+  attr_accessor :id, :first_name, :last_name, :email, :phone, :short_bio, :twitter, :blog_url, :resume_url, :github_url, :photo_url, :user_id, :educations, :experiences, :capstones, :skills
 
-
-
-
-
-
-
-
+  def initialize(student_data)
+    @id = student_data["id"]
+    @first_name = student_data["first_name"]
+    @last_name = student_data["last_name"]
+    @email = student_data["email"]
+    @phone = student_data["phone"]
+    @short_bio = student_data["short_bio"]
+    @twitter = student_data["twitter"]
+    @blog_url = student_data["blog_url"]
+    @resume_url = student_data["resume_url"]
+    @github_url = student_data["github_url"]
+    @photo_url = student_data["photo_url"]
+    @user_id = student_data["user_id"]
+    @educations = student_data["educations"]
+    @experiences = student_data["experiences"]
+    @capstones = student_data["capstones"]
+    @skills = student_data["skills"]
+  end
 
 
 
@@ -21,6 +33,13 @@ class Student
     student_data_hashes.each do |student_data_hash|
       students << Student.new(student_data_hash)
     end
+    students
+  end
+
+  def self.find_by(student_data)
+    id = student_data[:id]
+    student_data_hash = Unirest.get("http://172.28.113.208:3000/students/#{id}.json").body
+    Student.new(student_data_hash)
   end
 
 
